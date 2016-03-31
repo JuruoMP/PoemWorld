d73d0443448461454345d71f89799b43123ad86b
chenyuewang <ayano_39@163.com>
2016-03-31 12:41:27 +0800

    deploy mysql database

--- ReadingAssistant/models.py
+++ ReadingAssistant/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+#class author_table(models.Model):
+	#author_name = models.CharField(max_length=10)
+	#author_birth = models.DateField()
+	#author_death = models.DateField()
+	#author_desc = 
+	#author_head_thumb = models.CharField()
--- ReadingAssistantProject/settings.py
+++ ReadingAssistantProject/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'ReadingAssistantProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rd_db',
+        'USER': 'root',
+        'PASSWORD' : 'root',
+        'HOST' : '',
+        'PORT' : '',
     }
 }
 
